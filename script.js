ac76ecae033ef56d5bc5c4d4e41ef0b719f4af52
Hydeia <blakeyhydeia@gmail.com>
2021-10-19 16:57:11 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+class Flashcard {
+    constructor (card) {
+        this.card = card; 
+    }
+
+    
+    // event listener button for adding a new flashcard
+    addModal() {
+
+    }
+    //create a popup window after a button click
+    modal() {
+
+    }
+}
